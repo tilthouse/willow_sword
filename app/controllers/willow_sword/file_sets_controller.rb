ab456d99eaa4d07ed12d0510330be07909981204
Anusha Ranganathan <anusha3@gmail.com>
2019-06-12 03:29:52 +0100

    Moved methods into behaviour module to enable easy override of methods

--- app/controllers/willow_sword/file_sets_controller.rb
+++ app/controllers/willow_sword/file_sets_controller.rb
@@ -4,95 +4,10 @@ module WillowSword
   class FileSetsController < ApplicationController
     before_action :set_file_set_klass
     attr_reader :file_set, :object
+
     include WillowSword::ProcessRequest
     include WillowSword::WorksBehavior
     include WillowSword::FileSetsBehavior
-
-    def show
-      @file_set = find_file_set
-      render_file_set_not_found and return unless @file_set
-      urls = get_urls
-      xw_klass = WillowSword.config.xw_to_xml_for_fileset
-      xw = xw_klass.new(@file_set, urls)
-      xw.to_xml
-      @xml_data = xw.doc.to_s
-      render 'show.xml', formats: [:xml], status: 200
-    end
-
-    def create
-      # Find work
-      find_work_by_query(params[:work_id])
-      render_work_not_found and return unless @object
-      @error = nil
-      if perform_create
-        # @collection_id = params[:collection_id]
-        render 'create.xml.builder', formats: [:xml], status: :created,
-          location: collection_work_file_set_url(params[:collection_id], @object, @file_set)
-      else
-        @error = WillowSword::Error.new("Error creating file set") unless @error.present?
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-    end
-
-    def update
-      # Find work
-      find_work_by_query(params[:work_id])
-      render_work_not_found and return unless @object
-      # Find file set
-      @file_set = find_file_set
-      render_file_set_not_found and return unless @file_set
-      @error = nil
-      if perform_update
-        render 'update.xml.builder', formats: [:xml], status: :no_content
-      else
-        @error = WillowSword::Error.new("Error updating file set") unless @error.present?
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-
-    end
-
-    private
-      def perform_create
-        # If there are multiple files, the first one is picked
-        # If there are attributes, it is added to the file set
-        return false unless validate_and_save_request
-        if @files.blank?
-          message = "Content not received"
-          @error = WillowSword::Error.new(message)
-          return false
-        end
-        return false unless parse_metadata(@metadata_file, 'fileset')
-        create_file_set
-        true
-      end
-
-      def perform_update
-        # If there are multiple files, the first one is picked
-        # If there are attributes, it is added to the file set
-        return false unless validate_and_save_request
-        return false unless parse_metadata(@metadata_file, 'fileset', false)
-        update_file_set
-        true
-      end
-
-      def render_file_set_not_found
-        message = "Server cannot find file set with id #{params[:id]}"
-        @error = WillowSword::Error.new(message, :not_found)
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-
-      def render_work_not_found
-        message = "Server cannot find work with id #{params[:work_id]}"
-        @error = WillowSword::Error.new(message, :not_found)
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-
-      def get_urls
-        urls = {}
-        url = collection_work_file_set_url(params[:collection_id], params[:work_id], @file_set)
-        urls[@file_set.id] = { content: url, edit: url }
-        urls
-      end
-
+    include ::Integrator::Hyrax::FileSetsBehavior
   end
 end
--- app/controllers/willow_sword/works_controller.rb
+++ app/controllers/willow_sword/works_controller.rb
@@ -4,82 +4,9 @@ module WillowSword
   class WorksController < ApplicationController
     before_action :set_work_klass
     attr_reader :object, :current_user
+
     include WillowSword::ProcessRequest
     include WillowSword::WorksBehavior
-
-    def show
-      # @collection_id = params[:collection_id]
-      find_work_by_query
-      render_not_found and return unless @object
-      urls = get_urls
-      xw_klass = WillowSword.config.xw_to_xml_for_work
-      xw = xw_klass.new(@object, urls)
-      xw.to_xml
-      @xml_data = xw.doc.to_s
-      render 'show.xml', formats: [:xml], status: 200
-    end
-
-    def create
-      @error = nil
-      if perform_create
-        # @collection_id = params[:collection_id]
-        render 'create.xml.builder', formats: [:xml], status: :created, location: collection_work_url(params[:collection_id], @object)
-      else
-        @error = WillowSword::Error.new("Error creating work") unless @error.present?
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-    end
-
-    def update
-      # @collection_id = params[:collection_id]
-      find_work_by_query
-      render_not_found and return unless @object
-      @error = nil
-      if perform_update
-        render 'update.xml.builder', formats: [:xml], status: :no_content
-      else
-        @error = WillowSword::Error.new("Error updating work") unless @error.present?
-        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-      end
-    end
-
-    private
-
-    def perform_create
-      return false unless validate_and_save_request
-      return false unless parse_metadata(@metadata_file, 'work', true)
-      set_work_klass
-      upload_files unless @files.blank?
-      add_work
-      upload_files_with_attributes unless @files_attributes.blank?
-      true
-    end
-
-    def perform_update
-      return false unless validate_and_save_request
-      return false unless parse_metadata(@metadata_file, 'work', false)
-      upload_files unless @files.blank?
-      add_work
-      upload_files_with_attributes unless @files_attributes.blank?
-      true
-    end
-
-    def render_not_found
-      message = "Server cannot find work with id #{params[:id]}"
-      @error = WillowSword::Error.new(message, :not_found)
-      render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
-    end
-
-    def get_urls
-      urls = {}
-      url = collection_work_url(params[:collection_id], @object)
-      urls[@object.id] = { content: url, edit: url }
-      @object.file_sets.each do |file_set|
-        url = collection_work_file_set_url(params[:collection_id], @object, file_set)
-        urls[file_set.id] = { content: url, edit: url }
-      end
-      urls
-    end
-
+    include ::Integrator::Hyrax::WorksBehavior
   end
 end
